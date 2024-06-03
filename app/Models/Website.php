3510c287d3a9d1f61b55c7f570dfddb6147f8e66
matinTayebiNia <matintayebinia@gmail.com>
2024-06-03 15:34:54 +0330

    website migration and model ware implemented

--- app/Models/Website.php
+++ app/Models/Website.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Website extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "name",
+        "url",
+        "isHttps",
+        "ping",
+        "checked",
+        "status"
+    ];
+
+    /**
+     * getting info of user owner website
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2024_06_03_051452_create_websites_table.php
+++ database/migrations/2024_06_03_051452_create_websites_table.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('websites', function (Blueprint $table) {
             $table->id();
+            $table->string("name")->nullable();
+            $table->string("url");
+            $table->boolean("isHttps")->default(0);
+            $table->string("ping");
+            $table->boolean("status");
+            $table->boolean("checked")->default(0);
+            $table->foreignIdFor(User::class)->constrained()
+                ->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
         });
     }
